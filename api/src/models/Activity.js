6ffe66e470b49e20fdfea10aecd61485c9f86db8
devxyzr <develop.xyzr@gmail.com>
2022-11-09 18:48:43 -0500

    Create models database

---
+++ api/src/models/Activity.js
@@ -0,0 +1,36 @@
+const { sequelize, DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("activity", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isAlphanumeric: true,
+      },
+    },
+    difficulty: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 5,
+      },
+    },
+    lengthTime: {
+      type: DataTypes.INTEGER,
+    },
+    Temporada: {
+      type: DataTypes.ENUM("Verano", "Otoño", "Invierno", "Primavera"),
+      allowNull: false,
+    },
+  });
+};
---
+++ api/src/models/Country.js
@@ -0,0 +1,44 @@
+const { sequelize, DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("country", {
+    id: {
+      type: DataTypes.STRING(3),
+      primaryKey: true, // No se pone unique, ya que la primaryKey es unique
+      allowNull: false, // Allow Null  hace referencia a valores que no pueden quedar vacios
+    },
+    name: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    flagImage: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    subregion: {
+      type: DataTypes.STRING,
+    },
+    area: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    population: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  });
+};
